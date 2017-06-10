31a0059ba838d764362e67157ba87bb806ae4c3a
rhwy <github@rui.fr>
2017-06-10 02:05:37 +0200

    rc:next-define next dev objectives

---
+++ src/Humid.Tests/05_HandleBetterResponse.cs
@@ -0,0 +1,16 @@
+namespace Humid.Tests
+{
+    public class HandleBetterResponse
+    {
+        //actually we have a body response that is directly a string.
+        //what we want is to handle an object response and then serialize
+        //it as a string in the correct format depending on the accept type
+        //provided by the client.
+        //that means that we have to change:
+        //- add Headers in request
+        //- add headers in response
+        //- add an object response
+        //- change signature of Do webaction to Func<Context,object> and do the 
+        //   transformation to string while applying action
+    }
+}
