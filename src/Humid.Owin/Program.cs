6dc24b4cdfcbc29afca7d069ef8384bf1113522e
rhwy <github@rui.fr>
2017-06-16 03:01:08 +0200

    rc:exp-expering a first real life link with owin

---
+++ src/Humid.Owin/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using static Humid.WebActions;
+
+namespace Humid.Owin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+            Console.WriteLine("-------- Ready --------------");
+            host.Run();
+        }
+    }
+
+    public class Startup : IStartup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseHumid(routes => {
+                return routes 
+                + ( Get("/a") 
+                     | OK 
+                     | Content("I'm A"))
+                + ( Get("/b") 
+                     | OK 
+                     | Content("I'm B"));
+            });
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            return services.BuildServiceProvider();
+        }
+        
+    }
+}
