becd9c6268d7fbbc7a0da1c8f3e56a776918ad4f
rhwy <github@rui.fr>
2019-12-20 19:20:07 +0100

    rc:fix-missing experiment folder needed for tests

---
+++ _experimentdata/DynamicRouteModule/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using YamlDotNet;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace DynamicRouteModule
+{
+    public class Class1
+    {
+        public object ReadYaml(string content)
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            return deserializer.Deserialize<dynamic>(content);
+        }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Mail { get; set; }
+    }
+}
